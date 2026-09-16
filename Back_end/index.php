e1ffa594291d0f3a45a2833f9b1d891ac92c8a82
Luiz Miguel <Luizmigueofficer@gmail.com>
2026-09-16 01:04:48 -0300

    fix: add CORS headers for improved API accessibility

--- Back_end/index.php
+++ Back_end/index.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/src/Core/Main.php";
 
